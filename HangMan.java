94aa77bfd587482e2ef778816c3cc231f22f29cd
rin <116837475+rin-lang@users.noreply.github.com>
2023-02-13 00:25:11 -0500

    Add files via upload
    Added a while loop so you can play multiple times in one sitting.
    Received feedback that I should add more guesses, so I added that and updated the display of the hangman to align with the addition of more lives.
    Finished the game essentially.

--- HangMan.java
+++ HangMan.java
@@ -1,65 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.hangman;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author 24rla
- * 
- * Objective: creating my own version of the game, hangman
- * 
- * Methods:
- * 
- */
 public class HangMan {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException, IOException{
         
         //Level of Diffiulty
         //User can choose between easy, medium, hard, and expert modes for hang man
         //Easy = 1-6 characters, medium = 7-10 characters, hard = 11-15 characters, expert = 16+ 
+        
         Scanner s = new Scanner(System.in);
         int difficulty = 0;
-        while(true){
-            System.out.println("""
-                               Choose difficulty of game:
-                               Easy (1-6 letters)
-                               Medium (7-10 letters)
-                               Hard (11-15 letters)
-                               Expert (16+ letters)""");
-            String userInput1 = s.nextLine();
-            
-            if(userInput1.equalsIgnoreCase("easy")){
-                difficulty = 1;
-                break;
-            }
-            else if(userInput1.equalsIgnoreCase("medium")){
-                difficulty = 2;
-                break;
-            }
-            else if(userInput1.equalsIgnoreCase("hard")){
-                difficulty = 3;
-                break;
-            }
-            else if(userInput1.equalsIgnoreCase("expert")){
-                difficulty = 4;
-                break;
-            }
-            else{
-                System.out.println("That mode of difficulty does not currently exist.");
-            }
-        }//end of diffculty selection while loop
+        boolean play = true;
+        while(play){
+        do{
+                System.out.println("""
+                                   Choose difficulty of game:
+                                   Easy (1-6 letters)
+                                   Medium (7-10 letters)
+                                   Hard (11-15 letters)
+                                   Expert (16+ letters)""");
+                String userInput1 = s.nextLine();
+
+                if(userInput1.equalsIgnoreCase("easy")){
+                    difficulty = 1;
+                    break;
+                }
+                else if(userInput1.equalsIgnoreCase("medium")){
+                    difficulty = 2;
+                    break;
+                }
+                else if(userInput1.equalsIgnoreCase("hard")){
+                    difficulty = 3;
+                    break;
+                }
+                else if(userInput1.equalsIgnoreCase("expert")){
+                    difficulty = 4;
+                    break;
+                }
+                else{
+                    System.out.println("That mode of difficulty does not currently exist." + "\n");
+                }
+        }while(true);//end of diffculty selection while loop
        
         //Word Generation
         
@@ -67,41 +54,91 @@ public class HangMan {
         
         //Array and variable Creation
         
-        int tracker = 6; //keeps track of how many times the user has guessed
+        int tracker = 10; //keeps track of how many times the user has guessed
         //String[] fullWordLetters = HangManMethods.separateLetters(fullWord);//separate the random word given by the letters and put each letter into its own spot
+        String[] fullWord = new String[word.length()];
+        for(int q = 0; q < fullWord.length; q++){
+            fullWord[q] = (Character.toString(word.charAt(q)));
+        }
         String[] answerGiven = new String[word.length()];//create a blank array whose length is same as the word length
+        for(int i = 0; i < answerGiven.length; i++){
+            answerGiven[i] = "_ ";
+        }
         ArrayList<String> lettersGuessed = new ArrayList<>();//an array list of letters the user has guessed
         
-        //dealing with user guesses
-        while (HangManMethods.checkPoint(word, answerGiven) && tracker > 0){
-            System.out.println( "\n" + "Enter your guess: ");
-            String userInput2 = s.nextLine();
-            System.out.println("\n" + "Your Guess: " + userInput2);
-
-            //check if it's a possible guess, then check if it's been guessed before, then check if it's a correct guess
-            if(HangManMethods.possibleGuesses(userInput2)){
-                if(HangManMethods.userGuessed(userInput2, lettersGuessed)){
-                    System.out.println("You guessed that already, try something else." + "\n" + "You've Guessed: " + lettersGuessed + "\n");
-                }
-                else{
-                    if(HangManMethods.isItCorrect(userInput2, word)){
-                        System.out.println("That's right!" + "\n" + "You've Guessed: " + lettersGuessed + "\n" + "Lives Left: " + tracker);
-                        HangManMethods.displayHangMan(tracker, true);
-                    }//end of if it's a right guess
-                    else{
-                        tracker--;
-                        System.out.println("That's wrong!" + "\n" + "You've Guessed: " + lettersGuessed + "\n" +"Lives Left: " + tracker);
-                        HangManMethods.displayHangMan(tracker, false);
-                    }
-                }//end of if it's a wrong guess
-            }//end of if it is a letter
-            else{
-                System.out.println("That's not a letter... try again." +"\n");
-            }//end of if it isn't a letter
-
-        }//end of while loop for guesses
-  
         
+        
+        
+            //dealing with user guesses
+            while (HangManMethods.checkPoint(fullWord, answerGiven) && tracker > 0){
+                System.out.println( "\n" + "Enter your guess: ");
+                HangManMethods.displayHangMan(tracker);
+                
+                for( String answerGiven1: answerGiven){
+                    System.out.print(answerGiven1);
+                }
+                String userInput2 = s.nextLine();
+                System.out.println("\n" + "Your Guess: " + userInput2);
+
+                //check if it's a possible guess, then check if it's been guessed before, then check if it's a correct guess
+                if(HangManMethods.possibleGuesses(userInput2)){
+                    if(HangManMethods.userGuessed(userInput2, lettersGuessed)){
+                        System.out.println("You guessed that already, try something else." + "\n" + "You've Guessed: " + lettersGuessed + "\n");
+                    }
+                    else{
+                        if(HangManMethods.isItCorrect(userInput2, word)){
+                            System.out.println("That's right!" + "\n" + "You've Guessed: " + lettersGuessed + "\n" + "Lives Left: " + tracker);
+                            HangManMethods.displayHangMan(tracker);
+                            answerGiven = HangManMethods.wordDisplay(userInput2, word, answerGiven);
+                            for (String answerGiven1 : answerGiven) {
+                                System.out.print(answerGiven1);
+                            }
+                           // System.out.println("\t"+ word);
+                        }//end of if it's a right guess
+                        else{
+                            tracker--;
+                            System.out.println("That's wrong!" + "\n" + "You've Guessed: " + lettersGuessed + "\n" +"Lives Left: " + tracker);
+                            HangManMethods.displayHangMan(tracker);
+                            answerGiven = HangManMethods.wordDisplay(userInput2, word, answerGiven);
+                            for (String answerGiven1 : answerGiven) {
+                                System.out.print(answerGiven1);
+                            }
+                          //  System.out.println("\t"+word);
+                        }
+                    }//end of if it's a wrong guess
+                }//end of if it is a letter
+                else{
+                    System.out.println("That's not a letter ... try again." +"\n");
+                }//end of if it isn't a letter
+
+            }//end of while loop for guesses
+        if(Arrays.toString(fullWord).equals(Arrays.toString(answerGiven))){
+            System.out.println("\nYou win! The word was " + word + ".");
+        }
+        else if(tracker == 0){
+            System.out.println("\nGame Over..." + "\n" + "The word was " + word + ".");
+        }
+        
+        
+        System.out.println("Would you like to play again? [y/n]");
+        Scanner t = new Scanner(System.in);
+       
+        while(true){
+        String playAgain = t.nextLine();
+        if(playAgain.equalsIgnoreCase("yes") || playAgain.equalsIgnoreCase("y")){
+            play = true;
+            
+            break;
+        }
+        else if(playAgain.equalsIgnoreCase("no") || playAgain.equalsIgnoreCase("n")){
+            play = false;
+            break;
+        }
+        else{
+            System.out.println("That's not an answer, please type \"y\" for yes or \"n\" for no.");
+        }
+        }//end of asking to play again while loop
+    }//end of play again
     }//end of main method
 /*
 
--- HangManMethods.java
+++ HangManMethods.java
@@ -4,22 +4,52 @@
  */
 package com.mycompany.hangman;
 
-import java.util.ArrayList;
 import java.util.*;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.File;
+//import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
-/**
- *
- * @author 24rla
- */
+
 public class HangManMethods {
-        
-    public static String randomWord(int difficulty){
-        
-        return "testwordforfun";
+    
+    public static String randomWord(int difficulty) throws FileNotFoundException, MalformedURLException, IOException{
+        //File wordBank = new File("C:\\Users\\24rla\\OneDrive\\Desktop\\Hangman_WordBank.txt");
+        //Scanner x = new Scanner(new File("C:\\Users\\24rla\\OneDrive\\Desktop\\Hangman_WordBank.txt"));
+        java.net.URL url = new java.net.URL("http://www-personal.umich.edu/~jlawler/wordlist");
+        Scanner x = new Scanner(url.openStream());
+        ArrayList<String> wordBankEasy = new ArrayList<>();
+        ArrayList<String> wordBankMedium = new ArrayList<>();
+        ArrayList<String> wordBankHard = new ArrayList<>();
+        ArrayList<String> wordBankExpert = new ArrayList<>();
+        while(x.hasNext()){
+            String check = x.nextLine();
+            if(check.length() >= 1 && check.length() <= 6){
+                wordBankEasy.add(check);
+            }
+            else if(check.length() >= 7 && check.length() <= 10){
+                wordBankMedium.add(check);
+            }
+            else if(check.length() >= 11 && check.length() <= 15){
+                wordBankHard.add(check);
+            }
+            else if(check.length() >= 16){
+                wordBankExpert.add(check);
+            }
+        }//end of while
+        String word = "";
+        switch (difficulty) {
+            case 1 -> word = wordBankEasy.get((int)(Math.random() * wordBankEasy.size()));
+            case 2 -> word = wordBankMedium.get((int)(Math.random() * wordBankMedium.size()));
+            case 3 -> word = wordBankHard.get((int)(Math.random() * wordBankHard.size()));
+            case 4 -> word = wordBankExpert.get((int)(Math.random() * wordBankExpert.size()));
+           /* default -> {
+            }*/
+        }
+       return word;
+       //return "trythisword";
     }
     
     /*public static String[] separateLetters(String fullWord){
@@ -46,8 +76,8 @@ public class HangManMethods {
                                                                      "k", "l", "m", "n", "o",
                                                                      "p", "q", "r", "s", "t", 
                                                                      "u", "v", "w", "x", "y", 
-                                                                     "z"));
-        return alphabet.contains(userInput);
+                                                                     "z", "-"));
+        return alphabet.contains(userInput.toLowerCase());
     }
 
     
@@ -70,81 +100,181 @@ public class HangManMethods {
     
     
     //This method displays the actual hanging man
-    public static void displayHangMan(int tracker, boolean guess){
+    public static void displayHangMan(int tracker){
         
-        String six = """
+        String ten = """
                      ________
                      |      |
                      |      
                      |     
+                     |    
                      |     
+                     |     
+                     |    
                      |
                      """;
-        String five = """
-                      ________
-                      |      |
-                      |      O
-                      |     
-                      |     
-                      |
+        String nine = """
+                     ________
+                     |      |
+                     |      O
+                     |    
+                     |    
+                     |     
+                     |    
+                     |   
+                     |
                       """;
-        String four = """
+        String eight = """
                      ________
                      |      |
                      |      O
                      |      |
+                     |      | 
+                     |      |
                      |     
+                     |    
                      |
+                      """;
+        String seven = """
+                     ________
+                     |      |
+                     |      O
+                     |     /|
+                     |      | 
+                     |      |
+                     |     
+                     |    
+                     |
+                       """;
+        String six = """
+                     ________
+                     |      |
+                     |      O
+                     |     /|\\
+                     |      | 
+                     |      |
+                     |     
+                     |    
+                     |
+                     """;
+        String five = """
+                     ________
+                     |      |
+                     |      O
+                     |     /|\\
+                     |    o | 
+                     |      |
+                     |    
+                     |    
+                     |
+                     """;
+        String four = """
+                   ________
+                   |      |
+                   |      O
+                   |     /|\\
+                   |    o | o
+                   |      |
+                   |    
+                   |    
+                   |
                       """;
         String three = """
                      ________
                      |      |
                      |      O
-                     |     /|
-                     |     
+                     |     /|\\
+                     |    o | o
+                     |      |
+                     |     / 
+                     |      
                      |
                        """;
         String two = """
                      ________
                      |      |
                      |      O
-                     |     /|\
-                     |     
+                     |     /|\\
+                     |    o | o
+                     |      |
+                     |     / \\
+                     |    
                      |
-                     """;
-        String one = """
+                       """;
+        String one= """
                      ________
                      |      |
                      |      O
-                     |     /|\
-                     |     /
+                     |     /|\\
+                     |    o | o
+                     |      |
+                     |     / \\
+                     |    o   
                      |
-                     """;
+                       """;
         String zero = """
                      ________
                      |      |
                      |      O
-                     |     /|\
-                     |     / \
+                     |     /|\\
+                     |    o | o
+                     |      |
+                     |     / \\
+                     |    o   o
                      |
-                      """;
-        String outPut;
-        if(guess == true){
+                       """;
+                       
+        
+        String outPut = (tracker == 10 ? ten: tracker == 9 ? nine: tracker == 8 ? eight: tracker == 7 ? seven : tracker == 6 ? six : 
+                         tracker == 5 ? five : tracker == 4 ? four : tracker == 3 ? three : tracker == 2 ? two : tracker == 1 ? one : zero);
+        
+        /*if(guess == true){
             outPut = (tracker == 6 ? six : tracker == 5 ? five : tracker == 4 ? four : tracker == 3 ? three : tracker == 2 ? two : tracker == 1 ? one : zero);
         }
         else{
             outPut = (tracker == 6 ? six : tracker == 5 ? five : tracker == 4 ? four : tracker == 3 ? three : tracker == 2 ? two : tracker == 1 ? one : zero);
        
-        }
-        System.out.println(outPut);
+        }*/
+        System.out.print(outPut);
     }
+    
+    //This method checks the progress of how much of the word has been checked. 
+    //If the word has been fully and correctly guessed, it returns false to end the while loop.
+    public static boolean checkPoint(String[] fullWord, String[]answerGiven){
+        return !Arrays.toString(answerGiven).equals(Arrays.toString(fullWord));
+    }
+    
     
     //This method prints out what letters the user has guessed correctly in the word.
     //(e.g. e x _ m _ l e) 
-    public static boolean checkPoint(String word, String[]answerGiven){
-        
+    public static String[] wordDisplay(String userInput, String word, String[] answerGiven){
+        char temp = userInput.charAt(0);
+        if(isItCorrect(userInput, word)){
+            for(int k = 0; k < answerGiven.length; k++){
+                //System.out.println(temp);
+                    if(temp == word.charAt(k)){
+                        answerGiven[k] = userInput;
+                    }  
+               /* if(k == 0){
+                    if(userInput.equals(word.substring(0 , 1)))
+                    answerGiven[k] = userInput;
+                }
+                
+                else{
+                    if(userInput.equals(word.substring((k - 1), k))){
+                        answerGiven[k] = userInput;
+                    }  
+                }*/
+            }
+        }
+        return answerGiven;
+    }
+    
+    public static boolean wholeWordGuess(String userInput){
         return true;
     }
     
+    
+   // public static 
 }//end of class
 
